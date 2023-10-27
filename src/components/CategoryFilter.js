efce4b64f6831a4ce5107ccdfd21c7b43e6582e1
Neematrashid <nymatrasheed05@gmail.com>
2023-10-27 19:11:23 +0300

    almost !

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,9 +1,13 @@
 import React from "react";
 
-function CategoryFilter() {
+function CategoryFilter({category}) {
   return (
     <div className="categories">
       <h5>Category filters</h5>
+      <div>
+        {category.map((item) => (<button key = {item}>{item}</button>
+      ))}
+      </div>
       {/* render <button> elements for each category here */}
     </div>
   );
